e80c1df7a80a7ab6daa758e462279d380fa46ba9
danx0r <ubuntu@ip-172-31-19-194.us-west-2.compute.internal>
2020-02-09 00:07:09 +0000

    package of training data for balloon_test

---
+++ balloon_test_data/README.md
@@ -0,0 +1,2 @@
+mask_rcnn
+balloon test training data from https://github.com/matterport/Mask_RCNN 
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="balloon_test_data",
+    version="1.0.0",
+    packages=find_packages(),
+    python_requires='>=3',
+    include_package_data=True
+)
